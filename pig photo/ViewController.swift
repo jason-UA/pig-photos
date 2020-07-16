2ec2d1a5e6589ee9a7f95a34d0ccce1f46cf6478
Jason <jason.wang2@harman.com>
2020-07-16 13:55:24 +0800

    Initial Commit

---
+++ pig photo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pig photo
+//
+//  Created by jason on 2020/7/16.
+//  Copyright © 2020 jason. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
